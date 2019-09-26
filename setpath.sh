c2b99386b26f878e9d96b9aa46c46270ac6829f7
Jeroen <jeroenooms@gmail.com>
2019-09-26 10:55:17 +0200

    use safer variable

--- setpath.sh
+++ setpath.sh
@@ -1,5 +1,5 @@
 export PATH="$PATH:/C/progra~1/R/R-testing/bin:/C/progra~1/Pandoc:/c/progra~1/miktex 2.9/miktex/bin/x64:/C/progra~1/gs/gs9.27/bin:/C/GTK64/bin:/C/GTK32/bin"
-export R_LIBS="C:/R/library"
+export R_LIBS_USER="C:/R/library"
 export LIB_XML='/mingw${WIN}'
 export CLP_PATH='/mingw${WIN}'
 export _R_CHECK_FORCE_SUGGESTS_=0
