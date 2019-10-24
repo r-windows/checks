198754eafd5e977883885017ec78d18bc405a7e2
Windows <nobody@windows.com>
2019-10-24 14:33:09 +0200

    fix ghostscript path

--- setpath.sh
+++ setpath.sh
@@ -1,4 +1,4 @@
-export PATH="$PATH:/C/progra~1/R/R-testing/bin:/C/progra~1/Pandoc:/c/progra~1/miktex 2.9/miktex/bin/x64:/C/progra~1/gs/gs9.27/bin:/C/GTK64/bin:/C/GTK32/bin"
+export PATH="$PATH:/C/progra~1/R/R-testing/bin:/C/progra~1/Pandoc:/c/progra~1/miktex 2.9/miktex/bin/x64:/C/progra~1/gs/gs9.50/bin:/C/GTK64/bin:/C/GTK32/bin"
 export R_LIBS="C:/R/library"
 export LIB_XML='/mingw${WIN}'
 export CLP_PATH='/mingw${WIN}'
